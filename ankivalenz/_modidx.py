be1495ae244d98512fc7d1b1b07e0a3e3bae0a07
Harry Vangberg <harry@vangberg.name>
2022-11-02 15:25:22 +0100

    wip

--- ankivalenz/_modidx.py
+++ ankivalenz/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://vangberg.github.io',
                 'git_url': 'https://github.com/vangberg/ankivalenz',
                 'lib_path': 'ankivalenz'},
-  'syms': {'ankivalenz.core': {'ankivalenz.core.foo': ('core.html#foo', 'ankivalenz/core.py')}}}
+  'syms': { 'ankivalenz.ankivalenz': { 'ankivalenz.ankivalenz.foo': ('ankivalenz.html#foo', 'ankivalenz/ankivalenz.py'),
+                                       'ankivalenz.ankivalenz.say_hi': ('ankivalenz.html#say_hi', 'ankivalenz/ankivalenz.py')},
+            'ankivalenz.core': {'ankivalenz.core.foo': ('core.html#foo', 'ankivalenz/core.py')}}}
